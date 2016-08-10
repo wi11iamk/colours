2eec0117e4e74aa1f8d71f8973327871a2d1bdfd
liamwilt <liam1wilt@gmail.com>
2016-08-10 01:07:06 -0400

    Click functionality alterations.

--- main/colours.c
+++ main/colours.c
@@ -1,12 +1,13 @@
+#include<stdio.h>
 #include <gtk/gtk.h>
 
 int main(int argc, char *argv[]) {
 
   GtkWidget *window;
-  GtkWidget *vbox;
+  GtkWidget *box;
 
-  GtkWidget *line;
-  GtkWidget *func;
+  GtkWidget *ln;
+  GtkWidget *fx;
   GtkWidget *di;
   GtkWidget *ax;
 
@@ -14,22 +15,22 @@ int main(int argc, char *argv[]) {
 
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-  gtk_window_set_default_size(GTK_WINDOW(window), 230, 250);
+  gtk_window_set_default_size(GTK_WINDOW(window), 230, 500);
   gtk_window_set_title(GTK_WINDOW(window), "colourS");
   gtk_container_set_border_width(GTK_CONTAINER(window), 5);
 
-  vbox = gtk_vbox_new(TRUE, 1);
-  gtk_container_add(GTK_CONTAINER(window), vbox);
+  box = gtk_box_new(TRUE, 1);
+  gtk_container_add(GTK_CONTAINER(window), box);
 
   ln = gtk_button_new_with_label("Line");
   fx = gtk_button_new_with_label("Sine");
   di = gtk_button_new_with_label("Dice");
-  ax = gtk_button_new_with_label("Guitar");
+  ax = gtk_button_new_with_label("Axe");
 
-  gtk_box_pack_start(GTK_BOX(vbox), ln, TRUE, TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(vbox), fx, TRUE, TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(vbox), di, TRUE, TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(vbox), ax, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(box), ln, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(box), fx, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(box), di, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(box), ax, TRUE, TRUE, 0);
 
   g_signal_connect(G_OBJECT(window), "destroy",
         G_CALLBACK(gtk_main_quit), G_OBJECT(window));
